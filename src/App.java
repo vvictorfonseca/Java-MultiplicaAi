2d063071d8d25964f83f2682d12f50582cf23f01
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2023-01-11 16:45:31 -0300

    fix: sout output

--- src/App.java
+++ src/App.java
@@ -18,9 +18,7 @@ public class App {
       int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
       for(int i = 0; i < numbers.length; i++) {
-        System.out.println(numbers[i] * number);
+        System.out.println(number + " x " + numbers[i] + " = " + number * numbers[i]);
       }
-
-
     }
 }
